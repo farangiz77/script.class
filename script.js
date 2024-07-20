2b2b679e4cdbd43269d5cf3553e615cc4adbc484
farangiz77 <fireer4878@gmail.com>
2024-07-20 17:06:03 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,126 @@
+// let userName = "farangiz";
+// userName = "vit";
+
+// const myBirthday = "00.00.2000";
+
+// console.log(myBirthday);
+// alert(myBirthday);
+
+// let message = prompt("enter message");
+// console.log(message);
+
+// dannie
+
+// 1. number - chisla
+
+// 2. string - stroka 
+
+// 3. boolean 
+// true - pravda
+// false - loj
+
+// 4. null - nichego
+
+
+// 5. undefined - znachenie ne bilo prisvoeno
+
+// 6. object - obyekt - dlya xraneniya kollekciy razlichnix znacheniy
+// let user = { 
+//     name: "farangiz" , 
+//     age: 18,
+//     job: "krasotka",
+//     money: "10000$",
+// };
+
+// 7. bigint - dlya celix chisel proizvonoy dlini 
+
+// 8. symbol - unikalnix identifikatorov
+
+
+// let isStudy = confirm("ti uchishsya v IT?");
+
+// console.log(isStudy);
+
+
+
+// let userImage = null;  
+
+
+// let userName;
+
+// console.log(userName);
+
+// let userName1 = "farangiz"
+
+// 1)  +
+// 2)  -
+// 3)  *
+// 4)  /
+// 5)  % vzyat ostatok otdeleniya
+
+
+// console.log(5 % 2); // 1
+// console.log(8 % 3); //2
+// console.log(8 % 4); //0
+
+// 6) ** vozvedenie v stepen
+
+// console.log(2 ** 2); //4
+// console.log(2 ** 3); //8
+// console.log(2 ** 4); //16
+
+
+// inkrement ++ uvelichivaet peremennuyu na 1
+// dekrement -- umenshaet peremennuyu na 1
+
+// let num = 0;
+// console.log(++num);
+// console.log(--num);
+
+// operatori sravneniya
+// bilshe >
+// menshe <
+
+
+// bolshe libo ravno >=
+// menshe libo ravno <=
+
+
+// strogoe ravnestvo ===
+// ne strogoe ravnestvo ==
+
+// console.log(10 > 15); //false
+// console.log(10 < 15); //true
+// console.log(15 > 15); //false
+// console.log(15 <= 15); //true
+
+// console.log(15 === "15"); //false
+// console.log(15 == "15"); //true
+
+
+
+// let age = 15;
+
+// if (age < 18) {
+//     console.log("too little for this bruh");
+// } else {
+//     console.log("bolshoy");
+// }
+
+
+// let num1 = 10;
+// let num2 = 10;
+// let sign = prompt("vedite Math.znak")
+
+// if (sign === "-") {
+//     console.log(num1 - num2);
+
+// } else if (sign === "+"){
+//     console.log(num1 + num2);
+
+// } else if (sign === "*"){
+//     console.log(num1 * num2);
+
+// }else if (sign === "/") {
+//     console.log(num1 / num2);
+// }
